afeb26c15c05e01b633fe5e820a423c8a3f6c44e
xjli20 <xjli20@iflytek.com>
2023-09-07 18:43:26 +0800

    # 1 给代码添加注释

--- modules/ui.py
+++ modules/ui.py
@@ -1,44 +1,34 @@
-import random
 import time
 
-import os
-import typing
-
-from PyQt5 import QtCore
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QPixmap, QIcon, QPainter
-from PyQt5.QtWidgets import  QApplication,QMainWindow
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from . import settings
 from .model import Pet
+
 
 class DesktopPet(QMainWindow):
     """桌宠核心类"""
 
     def __init__(self):
         super(DesktopPet, self).__init__(None)
-        self.pet=Pet()
-        self.initUI()
-        self.play()
-
+        self.pet = Pet()  # 初始化宠物对象
+        self.initUI()  # 初始化界面
+        self.play()  # 开始播放动画
 
     def initUI(self):
-        self.desktop = QApplication.desktop()
+        self.desktop = QApplication.desktop()  # 获取桌面对象
         self.setWindowFlags(
-            Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint
+            Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint  # 设置窗口属性为无边框且置顶
         )
-        self.move(settings.INIT_POS_X,settings.INIT_POS_Y)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-        self.resize(settings.WINDOW_WIDTH,settings.WINDOW_HEIGHT)
+        self.move(settings.INIT_POS_X, settings.INIT_POS_Y)  # 设置窗口位置
+        self.setAttribute(Qt.WA_TranslucentBackground)  # 设置窗口背景透明
+        self.resize(settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT)  # 设置窗口大小
 
-
-    def move(self, ax: int, ay: int) -> None:
-        super().move(ax,ay)
-        self.pet.move(ax,ay)
-
-
-
-
+    def move(self, ax: int, ay: int) -> None:  # 移动窗口和宠物对象到指定位置
+        super().move(ax, ay)
+        self.pet.move(ax, ay)
 
     def mousePressEvent(self, event):
         pass
@@ -49,41 +39,29 @@ class DesktopPet(QMainWindow):
     def mouseMoveEvent(self, event):
         pass
 
-    def mouseDoubleClickEvent(self, QMouseEvent):
-        pass
+    def mouseDoubleClickEvent(self, QMouseEvent): pass  # 鼠标双击事件不做处理
+    def closeEvent(self, QCloseEvent): pass  # 关闭窗口事件不做处理
+    def contextMenuEvent(self, e): pass  # 右键菜单事件不做处理
 
-    def closeEvent(self, QCloseEvent):
-        pass
-
-    def contextMenuEvent(self, e):
-        pass
-
-    def paintEvent(self, QPaintEvent):
+    def paintEvent(self, QPaintEvent):  # 绘制窗口内容
         """绘图"""
-        painter = QPainter(self)
-        if hasattr(self, "pixmap"):
-            painter.drawPixmap(self.rect(), self.pixmap)
-        # painter.drawPixmap(self.rect(), self.pixmap)
+        painter = QPainter(self)  # 创建画笔对象
+        if hasattr(self, "pixmap"):  # 如果存在图片对象
+            painter.drawPixmap(self.rect(), self.pixmap)  # 在窗口上绘制图片
 
+    def play(self):  # 开始播放动画
+        self.timer = QTimer(self)  # 创建定时器对象
+        self.timer.timeout.connect(self.one_action)  # 当定时器超时时执行one_action方法
+        self.timer.start(0)  # 启动定时器，不设置时间间隔
 
-
-    def play(self):
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.one_action)
-        self.timer.start(0)
-
-    def one_action(self):
-        self.timer.stop()
-        action=self.pet.next_action() #type:BaseAction
-        for graph in action.graph_list:
-            self.pixmap = graph.pixmap
-            QApplication.processEvents()
-            self.update()
-            QApplication.processEvents()
-            time.sleep(graph.duration/1000)
-        # print("====")
-        self.pet.add_action()
-        self.timer.start()
-        # self.setPix(str(self.imgDir + "/摸头_000_125.png"))
-
-
+    def one_action(self):  # 每次动画帧的处理方法
+        self.timer.stop()  # 停止定时器
+        action = self.pet.next_action()  # 获取下一个动作对象
+        for graph in action.graph_list:  # 遍历动作中的图形列表
+            self.pixmap = graph.pixmap  # 将图形的图片对象赋值给窗口的图片对象属性
+            QApplication.processEvents()  # 让当前线程处理其他事件，如界面刷新等操作
+            self.update()  # 更新窗口显示内容
+            QApplication.processEvents()  # 让当前线程处理其他事件，如界面刷新等操作
+            time.sleep(graph.duration/1000)  # 根据图形的持续时间暂停一段时间后继续下一帧的绘制操作
+        self.pet.add_action()  # 将当前动作添加到宠物的动作列表中
+        self.timer.start()  # 继续播放动画
