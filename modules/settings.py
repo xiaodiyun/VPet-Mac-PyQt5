0179e4da05edceec02733c671fa24737d0bccc52
diyun <471293853@qq.com>
2023-09-11 08:34:18 +0800

    重构结束

--- modules/settings.py
+++ modules/settings.py
@@ -23,34 +23,34 @@ FALL_VY=[4,4]
 #普通无交互情况下，做事权重
 #TODO 考虑key改成元祖形式，这样可以更精细控制某些动作频率，以及屏蔽某些动作（设为0）
 COMMON_ACTION_WEIGHT={
-    ActionType.DEFAULT:1,
-    ActionType.MOVE:1,
+    ActionType.DEFAULT:40,
+    ActionType.MOVE:3,
     ActionType.STATEONE:5,
     ActionType.STATETWO:5,
-    ActionType.SAY:4,
-    ActionType.IDEL:10
+    ActionType.SAY:1,
+    ActionType.IDEL:8
 }
 #动作循环时，循环次数倾向
-# COMBO_ACTION_TIMES={
-#     ActionType.DEFAULT:[10,20],
-#     ActionType.MOVE:[10,50], #一直爬！爬到边缘被打断并进入climb
-#     ActionType.CLIMB:[30,30], #一直爬！爬到边缘被打断并进入climb
-#     ActionType.FALL:[30,30], #一般来说是落地之后自动打断，不存在循环次数
-#     ActionType.STATEONE:[30,60],
-#     ActionType.STATETWO:[30,60],
-#     ActionType.SAY:[2,4],
-#     ActionType.IDEL:[10,20], #很可爱！
-# }
-
 COMBO_ACTION_TIMES={
-    ActionType.DEFAULT:[2,2],
-    ActionType.MOVE:[2,2], #一直爬！爬到边缘被打断并进入climb
-    ActionType.CLIMB:[2,2], #一直爬！爬到边缘被打断并进入climb
-    ActionType.FALL:[2,2], #一般来说是落地之后自动打断，不存在循环次数
-    ActionType.STATEONE:[2,2],
-    ActionType.STATETWO:[2,2],
-    ActionType.SAY:[2,2],
-    ActionType.IDEL:[2,2], #很可爱！
+    ActionType.DEFAULT:[10,20],
+    ActionType.MOVE:[10,50], #一直爬！爬到边缘被打断并进入climb
+    ActionType.CLIMB:[30,30], #一直爬！爬到边缘被打断并进入climb
+    ActionType.FALL:[30,30], #一般来说是落地之后自动打断，不存在循环次数
+    ActionType.STATEONE:[30,60],
+    ActionType.STATETWO:[30,60],
+    ActionType.SAY:[2,4],
+    ActionType.IDEL:[10,20], #很可爱！
 }
+
+# COMBO_ACTION_TIMES={
+#     ActionType.DEFAULT:[2,2],
+#     ActionType.MOVE:[2,2], #一直爬！爬到边缘被打断并进入climb
+#     ActionType.CLIMB:[2,2], #一直爬！爬到边缘被打断并进入climb
+#     ActionType.FALL:[2,2], #一般来说是落地之后自动打断，不存在循环次数
+#     ActionType.STATEONE:[2,2],
+#     ActionType.STATETWO:[2,2],
+#     ActionType.SAY:[2,2],
+#     ActionType.IDEL:[2,2], #很可爱！
+# }
 
 
--- modules/ui.py
+++ modules/ui.py
@@ -1,48 +1,44 @@
 import random
 import time
 
-
-
 from PyQt5 import QtCore
-from PyQt5.QtCore import QTimer, Qt,QPoint
+from PyQt5.QtCore import QTimer, Qt, QPoint,QThread
 from PyQt5.QtGui import QPainter
-from PyQt5.QtWidgets import  QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from . import settings
 from .model import Pet
 from .dict import ActionType
+
 
 class DesktopPet(QMainWindow):
     """桌宠核心类"""
 
     def __init__(self):
         super(DesktopPet, self).__init__(None)
-        self.pet=Pet()
+        self.pet = Pet()
         self.initUI()
         self.play()
         self.last_graph_duration = 0
-        self.draggable=False
+        self.draggable = False
 
-
-        #长按也能触发提起
+        # 长按也能触发提起
         self.long_press_timer = QTimer()
         self.long_press_timer.setInterval(500)
         self.long_press_timer.timeout.connect(self.on_long_press)
-
 
     def initUI(self):
         self.desktop = QApplication.desktop()
         self.setWindowFlags(
             Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint
         )
-        self.move(QPoint(settings.INIT_POS_X,settings.INIT_POS_Y))
+        self.move(QPoint(settings.INIT_POS_X, settings.INIT_POS_Y))
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.resize(settings.WINDOW_WIDTH,settings.WINDOW_HEIGHT)
-
+        self.resize(settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT)
 
     def move(self, a0: QtCore.QPoint) -> None:
         super().move(a0)
-        #撞墙
+        # 撞墙
 
     def on_long_press(self):
         print("long_press")
@@ -59,7 +55,7 @@ class DesktopPet(QMainWindow):
             self.draggable = False
             self.long_press_timer.stop()
 
-    def mouseMoveEvent(self, event): #采样频率太低了，弃用
+    def mouseMoveEvent(self, event):  # 采样频率太低了，弃用
         print("move")
         if self.draggable:
             self.move(event.globalPos() - self.offset)
@@ -80,31 +76,25 @@ class DesktopPet(QMainWindow):
         if hasattr(self, "pixmap"):
             painter.drawPixmap(self.rect(), self.pixmap)
 
-
-
-
     def play(self):
         self.action_timer = QTimer(self)
         self.action_timer.timeout.connect(self.one_action)
-        self.action_timer.start() #是否要改成随机，待测试
+        self.action_timer.start()  # 是否要改成随机，待测试
 
 
 
     def one_action(self):
         self.action_timer.stop()
-        action=self.pet.next_action() #type:BaseAction
+        action = self.pet.next_action()  # type:BaseAction
         # if action.action_type==ActionType.MOVE:
 
-        for graph in action.graph_list: #如何立刻中断这里的动画，或者说需要打断这里吗？暂时不打断
+        for graph in action.graph_list:  # 如何立刻中断这里的动画，或者说需要打断这里吗？暂时不打断
             self.pixmap = graph.pixmap
             QApplication.processEvents()
             self.update()
             QApplication.processEvents()
-            time.sleep(self.last_graph_duration / 1000) #因为sleep的存在，所以move应当单独
-            self.last_graph_duration=graph.duration
-
-
-
+            time.sleep(self.last_graph_duration / 1000)  # 因为sleep的存在，所以move应当单独
+            self.last_graph_duration = graph.duration
 
         self.action_timer.start()
 
@@ -118,7 +108,7 @@ class DesktopPet(QMainWindow):
     hover，不知道干啥
     ui操作宠物 - 点击，播放touch动画
     ui操作宠物 - 走到屏幕边缘时，中断walk动画，播放climb动画
-    
+
 
 - 跟机器的事件监控
     监控操作宠物 - 放音乐时，播放music动作
@@ -128,4 +118,14 @@ class DesktopPet(QMainWindow):
 
 可能需要一个中间的触发器
 """
+
+
+class MyThread(QThread):
+    def run(self):
+        for i in range(10):
+            # 执行循环指令
+            print("Loop", i)
+
+            # 暂停2s
+            self.sleep(2)
 
